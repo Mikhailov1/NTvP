de4f17e0618e9a424a7765fc6d792f33ca74a05b
ALLIGATOR\Вячеслав <cc44613@gmail.com>
2016-12-05 18:07:54 +0700

    Удаление фрагмента кода

--- Discounts/View/Program.cs
+++ Discounts/View/Program.cs
@@ -14,9 +14,7 @@ namespace View
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DiscountsForm());
+            
         }
     }
 }
