b9997ed4a1780cd09a088a54ee94fb79605fd679
ALLIGATOR\Вячеслав <cc44613@gmail.com>
2016-12-05 18:08:40 +0700

    Восстановление удаленного фрагмента кода

--- Discounts/View/Program.cs
+++ Discounts/View/Program.cs
@@ -14,7 +14,9 @@ namespace View
         [STAThread]
         static void Main()
         {
-            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DiscountsForm());
         }
     }
 }
